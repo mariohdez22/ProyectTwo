2c2094daffd708fccae3fb1c1f96961944e75169
mariohdez22 <herzmariohdez@gmail.com>
2024-09-07 22:47:08 -0600

    Añadiendo modificaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatDividerModule} from "@angular/material/divider";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { InvoiceTypeComponent } from './invoice-type/invoice-type.component';
+import {MatButtonModule} from "@angular/material/button";
+import { AppRoutingModule } from './app-routing.module';
+import { CalculadoComponent } from './calculado/calculado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    InvoiceTypeComponent,
+    CalculadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDividerModule,
+    RouterOutlet,
+    MatButtonModule,
+    RouterLink,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
